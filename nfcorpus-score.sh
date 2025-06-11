5176931b97714913de655bdd8cf38042e62a3d16
Jacob Gorm Hansen <jhansen@dropbox.com>
2025-06-11 17:46:58 +0200

    call querycsv from scoring scripts

--- nfcorpus-score.sh
+++ nfcorpus-score.sh
@@ -1,1 +1,4 @@
-python score.py nfcorpus/output.txt /Users/jhansen/src/xtr-warp/beir/nfcorpus/collection_map.json /Users/jhansen/src/xtr-warp/beir/nfcorpus/qrels.test.json
+cargo run --release --features accelerate querycsv \
+    $HOME/src/xtr-warp/beir/nfcorpus/questions.test.tsv output.txt &&\
+
+python score.py nfcorpus/output.txt $HOME/src/xtr-warp/beir/nfcorpus/collection_map.json /Users/jhansen/src/xtr-warp/beir/nfcorpus/qrels.test.json
--- scifact-score.sh
+++ scifact-score.sh
@@ -1,1 +1,4 @@
-python score.py scifact/output.txt /Users/jhansen/src/xtr-warp/beir/scifact/collection_map.json /Users/jhansen/src/xtr-warp/beir/scifact/qrels.test.json
+cargo run --release --features accelerate querycsv \
+    $HOME/src/xtr-warp/beir/scifact/questions.test.tsv output.txt &&\
+
+python score.py scifact/output.txt $HOME/src/xtr-warp/beir/scifact/collection_map.json /Users/jhansen/src/xtr-warp/beir/scifact/qrels.test.json
